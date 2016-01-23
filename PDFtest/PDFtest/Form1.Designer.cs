098775e5ac6ff1163aeb6edd1c4d643f76e7a368
RunningEXE <runningexecutable@gmail.com>
2016-01-22 21:34:48 -0500

    Added mailing functionality

---
+++ PDFtest/PDFtest/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PDFtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnButtonPDF = new System.Windows.Forms.Button();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.mail = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnButtonPDF
+            // 
+            this.btnButtonPDF.Location = new System.Drawing.Point(69, 74);
+            this.btnButtonPDF.Name = "btnButtonPDF";
+            this.btnButtonPDF.Size = new System.Drawing.Size(75, 23);
+            this.btnButtonPDF.TabIndex = 0;
+            this.btnButtonPDF.Text = "Create PDF";
+            this.btnButtonPDF.UseVisualStyleBackColor = true;
+            this.btnButtonPDF.Click += new System.EventHandler(this.btnButtonPDF_Click);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(69, 24);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(100, 20);
+            this.txtInfo.TabIndex = 1;
+            // 
+            // mail
+            // 
+            this.mail.Location = new System.Drawing.Point(69, 144);
+            this.mail.Name = "mail";
+            this.mail.Size = new System.Drawing.Size(75, 23);
+            this.mail.TabIndex = 2;
+            this.mail.Text = "button1";
+            this.mail.UseVisualStyleBackColor = true;
+            this.mail.Click += new System.EventHandler(this.mail_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 238);
+            this.Controls.Add(this.mail);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.btnButtonPDF);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnButtonPDF;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Button mail;
+    }
+}
+
---
+++ PDFtest/PDFtest/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+using System.IO;
+//using System.Reflection;
+
+
+
+namespace PDFtest
+{
+    public partial class Form1 : Form
+    {
+        SaveFileDialog sfd = new SaveFileDialog();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnButtonPDF_Click(object sender, EventArgs e)
+        {
+        //path to save to your desktop
+            sfd.Title = "Save As PDF";
+            sfd.Filter = "PDF|.PDF";
+            sfd.InitialDirectory = @"C:\Users\RunningEXE\Desktop";
+
+        //pops up the dialog box to actually save
+            if (sfd.ShowDialog() == DialogResult.OK)
+            { 
+            Document doc = new Document(iTextSharp.text.PageSize.LETTER, 10, 10, 42, 35);
+            PdfWriter wri = PdfWriter.GetInstance(doc, new FileStream(sfd.FileName, FileMode.Create));
+            //pushes out notification when PDF is created
+            MessageBox.Show("PDF Saved to " + wri);
+            doc.Open();//Open Document to write
+            //write some content
+            Paragraph paragraph = new Paragraph(txtInfo.Text +" "+ "This is my firest line using paragraph. ");
+            //now add the above created text using different class object to our pdf document
+            doc.Add(paragraph);
+            doc.Close();//close document
+            }
+        }
+
+        /*private void mail_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<string> lstAllRecipients = new List<string>();
+
+                *******************************************************************************************
+                You can either hard-code the e-mails into the recipients Add function, or grab e-mails from
+                a database. I figured we could maybe hard-code them in? since idk how we'd get their e-mails through
+                a database. It would take us a bit more time, but it would be sort of neat to have all of the e-mails already
+                in there and ready to go.
+                ******************************************************************************************
+                lstAllRecipients.Add("");
+                lstAllRecipients.Add("");
+
+                Outlook.Application outlookApp = new Outlook.Application();
+                Outlook._MailItem oMailItem = (Outlook._MailItem)outlookApp.CreateItem(Outlook.OlItemType.olMailItem);
+                Outlook.Inspector oInspector = oMailItem.GetInspector;
+                // Thread.Sleep(10000);
+
+                //Recipient
+                Outlook.Recipients oRecips = (Outlook.Recipients)oMailItem.Recipients;
+                foreach (String recipient in lstAllRecipients)
+                {
+                    Outlook.Recipient oRecip = (Outlook.Recipient)oRecips.Add(recipient);
+                    oRecip.Resolve();
+                }
+
+                //Add CC
+                Outlook.Recipient oCCRecip = oRecips.Add("");
+                oCCRecip.Type = (int)Outlook.OlMailRecipientType.olCC;
+                oCCRecip.Resolve();
+
+                //Add Subject
+                oMailItem.Subject = "";
+
+                // body, bcc etc...
+
+                //Display the mailbox
+                oMailItem.Display(true);
+            }
+            catch (Exception objEx)
+            {
+                MessageBox.Show(objEx.ToString(),"");
+            }
+        }*/
+    }
+}
