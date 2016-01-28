e5048a5803a23ac6b676a255ed66056521f55769
RunningEXE <runningexecutable@gmail.com>
2016-01-28 13:29:36 -0500

    Started Design

--- DailyCensusReport/DailyCensusReport/Form1.cs
+++ DailyCensusReport/DailyCensusReport/Form1.cs
@@ -10,11 +10,12 @@ using System.Windows.Forms;
 
 namespace DailyCensusReport
 {
-    public partial class Form1 : Form
+    public partial class frmDailyCensusReport : Form
     {
-        public Form1()
+        public frmDailyCensusReport()
         {
             InitializeComponent();
         }
+
     }
 }
