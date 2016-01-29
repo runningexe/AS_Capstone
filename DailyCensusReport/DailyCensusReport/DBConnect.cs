d9509b7943f564a738974113368e138d6f2d0ec1
RunningEXE <runningexecutable@gmail.com>
2016-01-28 19:47:22 -0500

    Add Classes for Insert/Update/Get
    Database connection established.

---
+++ DailyCensusReport/DailyCensusReport/DBConnect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DailyCensusReport
+{
+    class DBConnect
+    {
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                "Data Source=sql.neit.edu;Initial Catalog=SE265_AJF1130;User ID=001246352  ;Password= 02272014";
+            SqlConnection cn = new SqlConnection(connectionString);
+            return cn;
+        }
+    }
+}
--- DailyCensusReport/DailyCensusReport/Form1.Designer.cs
+++ DailyCensusReport/DailyCensusReport/Form1.Designer.cs
@@ -45,6 +45,7 @@
             this.txtCCICU = new System.Windows.Forms.TextBox();
             this.rtbICUNotes = new System.Windows.Forms.RichTextBox();
             this.lblICU = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
             this.tlpHeaders.SuspendLayout();
             this.tlpICU.SuspendLayout();
             this.SuspendLayout();
@@ -59,7 +60,7 @@
             this.tlpHeaders.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 68F));
             this.tlpHeaders.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 74F));
             this.tlpHeaders.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 202F));
-            this.tlpHeaders.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 112F));
+            this.tlpHeaders.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 114F));
             this.tlpHeaders.Controls.Add(this.lblCurrentStatus, 6, 0);
             this.tlpHeaders.Controls.Add(this.lblNotes, 5, 0);
             this.tlpHeaders.Controls.Add(this.lblPotentialDCs, 4, 0);
@@ -80,7 +81,7 @@
             this.lblCurrentStatus.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblCurrentStatus.AutoSize = true;
             this.lblCurrentStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCurrentStatus.Location = new System.Drawing.Point(572, 2);
+            this.lblCurrentStatus.Location = new System.Drawing.Point(571, 2);
             this.lblCurrentStatus.Name = "lblCurrentStatus";
             this.lblCurrentStatus.Size = new System.Drawing.Size(101, 26);
             this.lblCurrentStatus.TabIndex = 6;
@@ -92,7 +93,7 @@
             this.lblNotes.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblNotes.AutoSize = true;
             this.lblNotes.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNotes.Location = new System.Drawing.Point(445, 9);
+            this.lblNotes.Location = new System.Drawing.Point(443, 9);
             this.lblNotes.Name = "lblNotes";
             this.lblNotes.Size = new System.Drawing.Size(40, 13);
             this.lblNotes.TabIndex = 5;
@@ -103,7 +104,7 @@
             this.lblPotentialDCs.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblPotentialDCs.AutoSize = true;
             this.lblPotentialDCs.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPotentialDCs.Location = new System.Drawing.Point(297, 2);
+            this.lblPotentialDCs.Location = new System.Drawing.Point(295, 2);
             this.lblPotentialDCs.Name = "lblPotentialDCs";
             this.lblPotentialDCs.Size = new System.Drawing.Size(57, 26);
             this.lblPotentialDCs.TabIndex = 4;
@@ -115,7 +116,7 @@
             this.lblIsolationPts.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblIsolationPts.AutoSize = true;
             this.lblIsolationPts.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblIsolationPts.Location = new System.Drawing.Point(226, 1);
+            this.lblIsolationPts.Location = new System.Drawing.Point(224, 1);
             this.lblIsolationPts.Name = "lblIsolationPts";
             this.lblIsolationPts.Size = new System.Drawing.Size(55, 29);
             this.lblIsolationPts.TabIndex = 3;
@@ -127,7 +128,7 @@
             this.lblAvailBeds.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblAvailBeds.AutoSize = true;
             this.lblAvailBeds.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAvailBeds.Location = new System.Drawing.Point(166, 2);
+            this.lblAvailBeds.Location = new System.Drawing.Point(164, 2);
             this.lblAvailBeds.Name = "lblAvailBeds";
             this.lblAvailBeds.Size = new System.Drawing.Size(39, 26);
             this.lblAvailBeds.TabIndex = 2;
@@ -139,7 +140,7 @@
             this.lblCurrentCensus.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblCurrentCensus.AutoSize = true;
             this.lblCurrentCensus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCurrentCensus.Location = new System.Drawing.Point(92, 2);
+            this.lblCurrentCensus.Location = new System.Drawing.Point(90, 2);
             this.lblCurrentCensus.Name = "lblCurrentCensus";
             this.lblCurrentCensus.Size = new System.Drawing.Size(48, 26);
             this.lblCurrentCensus.TabIndex = 1;
@@ -151,7 +152,7 @@
             this.lblUnit.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblUnit.AutoSize = true;
             this.lblUnit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUnit.Location = new System.Drawing.Point(22, 9);
+            this.lblUnit.Location = new System.Drawing.Point(21, 9);
             this.lblUnit.Name = "lblUnit";
             this.lblUnit.Size = new System.Drawing.Size(37, 13);
             this.lblUnit.TabIndex = 0;
@@ -168,7 +169,7 @@
             this.tlpICU.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 69F));
             this.tlpICU.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 73F));
             this.tlpICU.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 202F));
-            this.tlpICU.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 112F));
+            this.tlpICU.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 116F));
             this.tlpICU.Controls.Add(this.txtCCSICU, 6, 0);
             this.tlpICU.Controls.Add(this.txtICUDC, 4, 0);
             this.tlpICU.Controls.Add(this.txtISOICU, 3, 0);
@@ -186,42 +187,42 @@
             // 
             // txtCCSICU
             // 
-            this.txtCCSICU.Location = new System.Drawing.Point(569, 4);
+            this.txtCCSICU.Location = new System.Drawing.Point(565, 4);
             this.txtCCSICU.Name = "txtCCSICU";
             this.txtCCSICU.Size = new System.Drawing.Size(105, 20);
             this.txtCCSICU.TabIndex = 6;
             // 
             // txtICUDC
             // 
-            this.txtICUDC.Location = new System.Drawing.Point(292, 4);
+            this.txtICUDC.Location = new System.Drawing.Point(288, 4);
             this.txtICUDC.Name = "txtICUDC";
             this.txtICUDC.Size = new System.Drawing.Size(67, 20);
             this.txtICUDC.TabIndex = 4;
             // 
             // txtISOICU
             // 
-            this.txtISOICU.Location = new System.Drawing.Point(222, 4);
+            this.txtISOICU.Location = new System.Drawing.Point(218, 4);
             this.txtISOICU.Name = "txtISOICU";
             this.txtISOICU.Size = new System.Drawing.Size(63, 20);
             this.txtISOICU.TabIndex = 3;
             // 
             // txtABICU
             // 
-            this.txtABICU.Location = new System.Drawing.Point(153, 4);
+            this.txtABICU.Location = new System.Drawing.Point(149, 4);
             this.txtABICU.Name = "txtABICU";
             this.txtABICU.Size = new System.Drawing.Size(62, 20);
             this.txtABICU.TabIndex = 2;
             // 
             // txtCCICU
             // 
-            this.txtCCICU.Location = new System.Drawing.Point(84, 4);
+            this.txtCCICU.Location = new System.Drawing.Point(82, 4);
             this.txtCCICU.Name = "txtCCICU";
-            this.txtCCICU.Size = new System.Drawing.Size(62, 20);
+            this.txtCCICU.Size = new System.Drawing.Size(60, 20);
             this.txtCCICU.TabIndex = 0;
             // 
             // rtbICUNotes
             // 
-            this.rtbICUNotes.Location = new System.Drawing.Point(366, 4);
+            this.rtbICUNotes.Location = new System.Drawing.Point(362, 4);
             this.rtbICUNotes.Name = "rtbICUNotes";
             this.rtbICUNotes.Size = new System.Drawing.Size(196, 21);
             this.rtbICUNotes.TabIndex = 7;
@@ -232,23 +233,43 @@
             this.lblICU.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblICU.AutoSize = true;
             this.lblICU.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblICU.Location = new System.Drawing.Point(26, 8);
+            this.lblICU.Location = new System.Drawing.Point(25, 8);
             this.lblICU.Name = "lblICU";
             this.lblICU.Size = new System.Drawing.Size(28, 13);
             this.lblICU.TabIndex = 8;
             this.lblICU.Text = "ICU";
             this.lblICU.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // btnSubmit
+            // 
+            this.btnSubmit.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnSubmit.FlatAppearance.BorderSize = 3;
+            this.btnSubmit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSubmit.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSubmit.Location = new System.Drawing.Point(12, 308);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(213, 40);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
             // frmDailyCensusReport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(704, 360);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(712, 441);
+            this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.tlpICU);
             this.Controls.Add(this.tlpHeaders);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmDailyCensusReport";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Daily Census Report";
+            this.Load += new System.EventHandler(this.frmDailyCensusReport_Load);
             this.tlpHeaders.ResumeLayout(false);
             this.tlpHeaders.PerformLayout();
             this.tlpICU.ResumeLayout(false);
@@ -275,6 +296,7 @@
         private System.Windows.Forms.TextBox txtCCICU;
         private System.Windows.Forms.RichTextBox rtbICUNotes;
         private System.Windows.Forms.Label lblICU;
+        private System.Windows.Forms.Button btnSubmit;
     }
 }
 
