7e1c19028e3cdde6eb3d5c92edbf10c4d68a12cb
RunningEXE <runningexecutable@gmail.com>
2016-02-07 12:54:18 -0500

    Going to re-do view record stuff

--- DailyCensusReport/DailyCensusReport/ViewRecordsForm.cs
+++ DailyCensusReport/DailyCensusReport/ViewRecordsForm.cs
@@ -16,48 +16,38 @@ namespace DailyCensusReport
             InitializeComponent();
         }
 
-        private void iCUBindingNavigatorSaveItem_Click(object sender, EventArgs e)
-        {
-            this.Validate();
-            this.iCUBindingSource.EndEdit();
-            this.tableAdapterManager.UpdateAll(this.sE265_AJF1130DataSet);
-
-        }
-
         private void ViewRecordsForm_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'sE265_AJF1130DataSet.ICU' table. You can move, or remove it, as needed.
-            this.iCUTableAdapter.Fill(this.sE265_AJF1130DataSet.ICU);
 
         }
 
-//DateTime Picker
-        private void  dateTimePicker1_ValueChanged(object sender, EventArgs e)
-        {
-            DataTable dt = new DataTable();
+        //DateTime Picker
+        //private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        //{
+        //    DataTable dt = new DataTable();
 
-            SqlConnection cn = DBConnect.GetConnection();
+        //    SqlConnection cn = DBConnect.GetConnection();
 
-            SqlCommand cmd = new SqlCommand("spCensus", cn);
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.Add("@date", SqlDbType.VarChar).Value = DPRecordDate.Value;
-            
+        //    SqlCommand cmd = new SqlCommand("spCensus", cn);
+        //    cmd.CommandType = CommandType.StoredProcedure;
+        //    cmd.Parameters.Add("@date", SqlDbType.VarChar).Value = DPRecordDate.Value;
 
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            try
-            {
-                cn.Open();
-                da.Fill(dt);
-            }
-            catch (Exception ex)
-            {
 
-            }
-            finally
-            {
-                cn.Close();
-            }
-            RecordView.Rows.Add(dt.Rows[0]);
-        }
+        //    SqlDataAdapter da = new SqlDataAdapter(cmd);
+        //    try
+        //    {
+        //        cn.Open();
+        //        da.Fill(dt);
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //    }
+        //    finally
+        //    {
+        //        cn.Close();
+        //    }
+        //    RecordView.Rows.Add(dt.Rows[0]);
+        //}
     }
 }
