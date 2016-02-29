1acd1cfe59b6eefdfd0207768c1ce2a34463e590
RunningEXE <runningexecutable@gmail.com>
2016-02-29 16:02:06 -0500

    Help page is cool

--- DailyCensusReport/DailyCensusReport/HelpForm.Designer.cs
+++ DailyCensusReport/DailyCensusReport/HelpForm.Designer.cs
@@ -32,13 +32,19 @@
             this.btnNext = new System.Windows.Forms.Button();
             this.lblHelpHeader = new System.Windows.Forms.Label();
             this.btnBack = new System.Windows.Forms.Button();
-            this.lblHelp = new System.Windows.Forms.Label();
+            this.pnlPage1 = new System.Windows.Forms.Panel();
+            this.pnlPage2 = new System.Windows.Forms.Panel();
+            this.lblPg1 = new System.Windows.Forms.Label();
+            this.lblPg2 = new System.Windows.Forms.Label();
+            this.lblSubmittingInfoHelp = new System.Windows.Forms.Label();
+            this.pnlPage1.SuspendLayout();
+            this.pnlPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnNext
             // 
             this.btnNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnNext.Location = new System.Drawing.Point(389, 408);
+            this.btnNext.Location = new System.Drawing.Point(412, 408);
             this.btnNext.Name = "btnNext";
             this.btnNext.Size = new System.Drawing.Size(120, 30);
             this.btnNext.TabIndex = 1;
@@ -50,7 +56,7 @@
             // 
             this.lblHelpHeader.AutoSize = true;
             this.lblHelpHeader.Font = new System.Drawing.Font("Oswald Regular", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHelpHeader.Location = new System.Drawing.Point(27, 30);
+            this.lblHelpHeader.Location = new System.Drawing.Point(15, 20);
             this.lblHelpHeader.Name = "lblHelpHeader";
             this.lblHelpHeader.Size = new System.Drawing.Size(174, 36);
             this.lblHelpHeader.TabIndex = 0;
@@ -59,7 +65,7 @@
             // btnBack
             // 
             this.btnBack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnBack.Location = new System.Drawing.Point(263, 408);
+            this.btnBack.Location = new System.Drawing.Point(286, 408);
             this.btnBack.Name = "btnBack";
             this.btnBack.Size = new System.Drawing.Size(120, 30);
             this.btnBack.TabIndex = 2;
@@ -67,24 +73,59 @@
             this.btnBack.UseVisualStyleBackColor = true;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
-            // lblHelp
+            // pnlPage1
             // 
-            this.lblHelp.AutoSize = true;
-            this.lblHelp.Location = new System.Drawing.Point(30, 97);
-            this.lblHelp.Name = "lblHelp";
-            this.lblHelp.Size = new System.Drawing.Size(0, 13);
-            this.lblHelp.TabIndex = 1;
-            this.lblHelp.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.pnlPage1.Controls.Add(this.lblPg1);
+            this.pnlPage1.Controls.Add(this.lblHelpHeader);
+            this.pnlPage1.Location = new System.Drawing.Point(23, 12);
+            this.pnlPage1.Name = "pnlPage1";
+            this.pnlPage1.Size = new System.Drawing.Size(493, 378);
+            this.pnlPage1.TabIndex = 3;
+            // 
+            // pnlPage2
+            // 
+            this.pnlPage2.Controls.Add(this.lblSubmittingInfoHelp);
+            this.pnlPage2.Controls.Add(this.lblPg2);
+            this.pnlPage2.Location = new System.Drawing.Point(23, 12);
+            this.pnlPage2.Name = "pnlPage2";
+            this.pnlPage2.Size = new System.Drawing.Size(493, 378);
+            this.pnlPage2.TabIndex = 4;
+            // 
+            // lblPg1
+            // 
+            this.lblPg1.AutoSize = true;
+            this.lblPg1.Location = new System.Drawing.Point(23, 71);
+            this.lblPg1.Name = "lblPg1";
+            this.lblPg1.Size = new System.Drawing.Size(0, 13);
+            this.lblPg1.TabIndex = 1;
+            // 
+            // lblPg2
+            // 
+            this.lblPg2.AutoSize = true;
+            this.lblPg2.Location = new System.Drawing.Point(22, 71);
+            this.lblPg2.Name = "lblPg2";
+            this.lblPg2.Size = new System.Drawing.Size(0, 13);
+            this.lblPg2.TabIndex = 2;
+            // 
+            // lblSubmittingInfoHelp
+            // 
+            this.lblSubmittingInfoHelp.AutoSize = true;
+            this.lblSubmittingInfoHelp.Font = new System.Drawing.Font("Oswald Regular", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubmittingInfoHelp.Location = new System.Drawing.Point(19, 19);
+            this.lblSubmittingInfoHelp.Name = "lblSubmittingInfoHelp";
+            this.lblSubmittingInfoHelp.Size = new System.Drawing.Size(241, 36);
+            this.lblSubmittingInfoHelp.TabIndex = 2;
+            this.lblSubmittingInfoHelp.Text = "Submitting Information";
             // 
             // HelpForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(516, 447);
-            this.Controls.Add(this.lblHelp);
+            this.ClientSize = new System.Drawing.Size(539, 447);
             this.Controls.Add(this.btnBack);
-            this.Controls.Add(this.lblHelpHeader);
+            this.Controls.Add(this.pnlPage1);
             this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.pnlPage2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -92,8 +133,11 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Help";
             this.Load += new System.EventHandler(this.HelpForm_Load);
+            this.pnlPage1.ResumeLayout(false);
+            this.pnlPage1.PerformLayout();
+            this.pnlPage2.ResumeLayout(false);
+            this.pnlPage2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -101,6 +145,10 @@
         private System.Windows.Forms.Button btnNext;
         private System.Windows.Forms.Label lblHelpHeader;
         private System.Windows.Forms.Button btnBack;
-        private System.Windows.Forms.Label lblHelp;
+        private System.Windows.Forms.Panel pnlPage1;
+        private System.Windows.Forms.Panel pnlPage2;
+        private System.Windows.Forms.Label lblPg1;
+        private System.Windows.Forms.Label lblPg2;
+        private System.Windows.Forms.Label lblSubmittingInfoHelp;
     }
 }
--- DailyCensusReport/DailyCensusReport/HelpForm.cs
+++ DailyCensusReport/DailyCensusReport/HelpForm.cs
@@ -20,25 +20,28 @@ namespace DailyCensusReport
         private void btnNext_Click(object sender, EventArgs e)
         {
             btnBack.Show();
-            lblHelpHeader.Text = "Submitting Information";
-            lblHelp.Text = "Upon loading the program you will be greeted with the form to submit records into the database. \n"
+            lblPg2.Text = "Upon loading the program you will be greeted with the form to submit records into the database. \n"
                          + "You will have to fill out all of the fields with information that cannot exceed the maximum \n" 
                          + "number of beds for each unit. \n\n The maximum number of beds for each unit are: \n\n"
                          + "            -ICU [9 Beds] \n" + "            -T2 [28 Beds] \n" + "            -PEDI [28 Beds] \n" + "            -T4 [29 Beds] \n" + "            -6ACU [10 Beds] \n" + "            -TBC [10 Beds] \n" + "            -BHU [15 Beds] \n\n"
                          + "Whatever number you put in for the Current Census for each unit, the number \n of available beds "
                          + "will change dynamically to the proper amount of remaining/available beds.";
+            pnlPage1.SendToBack();
+            pnlPage2.BringToFront();
         }
 
         private void HelpForm_Load(object sender, EventArgs e)
         {          
             btnBack.Hide();
-            lblHelp.Text = "If you are having trouble figuring out how the program works, please refer to the next few pages."
+            lblHelpHeader.Text = "Getting Started...";
+            lblPg1.Text = "If you are having trouble figuring out how the program works, please refer to the next few pages."
                                 + "\n\n\n\n\n\n Click 'Next' to read further.";
         }
 
         private void btnBack_Click(object sender, EventArgs e)
         {
-
+            
+            pnlPage2.SendToBack();
         }
     }
 }
